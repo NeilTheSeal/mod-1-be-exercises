ec6011bee6a7541414b85ed8c47c18af1f8c2fb4
Neil Hendren <neil.hendren@gmail.com>
2024-01-31 10:53:59 -0700

    More linting debugging

--- lessons/class_methods/errors.rb
+++ lessons/class_methods/errors.rb
@@ -1,8 +1,9 @@
 # What does an error look like when using Class Methods?
-require 'pry'
+require "pry"
 
 class Dog
   attr_reader :name
+
   def initialize(name)
     @name = name
   end
@@ -12,10 +13,9 @@ class Dog
   end
 
   def self.bark
-    #binding.pry ## Can we access @name from this class method? What will output next?
+    # binding.pry ## Can we access @name from this class method? What will output next?
     puts "#{@name} says, 'bark'."
   end
-
 end
 
 fluffy = Dog.new("Fluffy")
@@ -25,8 +25,8 @@ fluffy.bark
 ## Uncomment each line of code, one at a time. What is the difference between teh two errors?
 ## What specific data type is each error on - are they instances or classes?
 
-#fluffy.woof
+# fluffy.woof
 # => undefined method "woof" for #<Dog:0x00....> (NoMethodError)
 
-#Dog.woof
+# Dog.woof
 # => undefined method "woof" for Dog:Class
