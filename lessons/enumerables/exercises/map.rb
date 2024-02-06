d07d5a76355afc6de9066c3c066e09cc5e1c3464
NeilTheSeal <neil.hendren@gmail.com>
2024-02-06 09:40:17 -0700

    Complete map exercise

--- lessons/enumerables/exercises/map.rb
+++ lessons/enumerables/exercises/map.rb
@@ -1,16 +1,9 @@
-# The method below returns an array of the brothers names in all caps using the method #each.
-# # Your Task: Update this method to use #map. 
+# The method below returns an array of the brothers names in all caps using the
+# method #each. Your Task: Update this method to use #map.
 
 def kennedy_brothers
-    brothers = ["Robert", "Ted", "Joseph", "John"]
-  
-    caps_brothers = []
-  
-    brothers.each do |brother|
-      caps_brothers << brother.upcase
-    end
-  
-    caps_brothers
+  brothers = %w[Robert Ted Joseph John]
+  brothers.map(&:upcase)
 end
 
 p kennedy_brothers
