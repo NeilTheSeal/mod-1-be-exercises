64a5c2faa12e3120d668ed494e21e82070ff1f7b
NeilTheSeal <neil.hendren@gmail.com>
2024-02-06 09:28:45 -0700

    Complete enumerables warm-up

--- lessons/enumerables/warm_up/warm_up.rb
+++ lessons/enumerables/warm_up/warm_up.rb
@@ -1,48 +1,45 @@
-# 1. Using #each, add to the method below, such that it takes in an array of names, 
+# 1. Using #each, add to the method below, such that it takes in an array of names,
 # and returns an array of all names upcased.
 
-kardashians = ["kris", "kim", "kourtney", "khloe", "rob"]
+kardashians = %w[kris kim kourtney khloe rob]
 
 def upcase_names(names)
- # your code here
-end 
+  # your code here
+  names.each(&:upcase!)
+end
 
 p upcase_names(kardashians)
 
 # should return ["KRIS", "KIM", "KOURTNEY", "KHLOE", "ROB"]
 
+# 2. Using #each, update the method below, such that it takes in an array of
+# names, and returns a collection of the names that end in 'ie'
 
-
-
-
-
-# 2. Using #each, update the method below, such that it takes in an array of names,
-# and returns a collection of the names that end in 'ie'
-
-pets = ["Brutus", "Lucky", "Goldie", "Pepper", "Odie"]
+pets = %w[Brutus Lucky Goldie Pepper Odie]
 
 def names_ending_in_ie(names)
- # your code here
-end 
+  pets_ie = []
+  names.each do |name|
+    pets_ie.push(name) if name[-2, 2] == "ie"
+  end
+  pets_ie
+end
 
 p names_ending_in_ie(pets)
 
 # should return an array of ["Goldie", "Odie"]
 
-
-
-
-
-
-# 3. Using #each, update the method below, such that it takes in an array of integers,
-# and returns the first number that is over 100 when squared. 
+# 3. Using #each, update the method below, such that it takes in an array of
+# integers, and returns the first number that is over 100 when squared.
 
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-def first_num_thats_square_is_above_100(nums)
+def first_num_thats_square_is_above100(nums)
+  nums.each do |num|
+    return num if num**2 > 100
+  end
+end
 
-end 
-
-p first_num_thats_square_is_above_100(nums)
+p first_num_thats_square_is_above100(nums)
 
 # should return the integer 11
