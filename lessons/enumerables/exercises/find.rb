cbed6f0edc44bfebb4d59350e9a6762677e5bd22
NeilTheSeal <neil.hendren@gmail.com>
2024-02-06 09:36:44 -0700

    Complete find exercise

--- lessons/enumerables/exercises/find.rb
+++ lessons/enumerables/exercises/find.rb
@@ -1,14 +1,13 @@
-# 1. Using #each, write the code to find the first sister over four letters in length.
-# 2. Once you get the method below to work using #each, refactor your code to use #find. 
+# 1. Using #each, write the code to find the first sister over four letters in
+# length.
+# 2. Once you get the method below to work using #each, refactor your code to
+# use #find.
 
 def find_first_long_named_sister
-    sisters = ["Rose", "Kathleen", "Eunice", "Patricia", "Jean"]
-  
-    ### YOUR CODE HERE
-
-    # should return "Kathleen"
+  sisters = %w[Rose Kathleen Eunice Patricia Jean]
+  sisters.find do |name|
+    name.length > 4
+  end
 end
-
-
 
 p find_first_long_named_sister
