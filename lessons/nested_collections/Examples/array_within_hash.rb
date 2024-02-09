7f7324691199ecfa7a55bd2b89e7e388c0c0ed46
NeilTheSeal <neil.hendren@gmail.com>
2024-02-09 09:48:42 -0700

    Finish nested collections examples

--- lessons/nested_collections/Examples/array_within_hash.rb
+++ lessons/nested_collections/Examples/array_within_hash.rb
@@ -1,10 +1,20 @@
 pizza_toppings = {
   veggies: ["green peppers", "jalapeño", "mushrooms"],
-  protein: ["pepperoni", "sausage", "sardines"],
+  protein: %w[pepperoni sausage sardines],
   fruit: ["pineapple"]
 }
 
 # 1. what is pizza_toppings.count
+# => 3
 # 2. what is pizza_toppings.values
-# 3. how can I access the element “pineapple”
+# => [
+# ["green peppers", "jalapeño", "mushrooms"],
+# ["pepperoni", "sausage", "sardines"],
+# ["pineapple"]
+# ]
+# 3. how can I access the element “pineapple"
+# => pizza_toppings[:fruit][0]
 # 4. how can I add the element “olives” to the key “veggies”?
+# => pizza_toppings[:veggies] << "olives"
+
+pizza_toppings
--- lessons/nested_collections/Examples/warm_up.rb
+++ lessons/nested_collections/Examples/warm_up.rb
@@ -1,10 +1,21 @@
-animals = {"dogs" => 3, "cats" => 5, "iguanas" => 2}
+animals = { "dogs" => 3, "cats" => 5, "iguanas" => 2 }
 
 # 1. return the value of dogs
 # 2. add 3 parakeets
 # 3. increase the amount of cats by 2
 
-pet_names = ["Fela", "Spot", "Patch", "Willy"]
+# puts animals["dogs"]
+animals["parakeet"] = 3
+# p animals
+animals["cats"] = animals["cats"] + 2
+# p animals
+
+pet_names = %w[Fela Spot Patch Willy]
+pet_names << "Claude"
+p pet_names[0]
+p pet_names.last
+pet_names.shift
+p pet_names
 
 # 1. add “Claude” to the end of the array
 # 2. access the first name in the list
