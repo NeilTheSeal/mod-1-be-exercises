b6fc8f614882a2a5e8bc60037b6eb7a8ad36d2df
NeilTheSeal <neil.hendren@gmail.com>
2024-02-13 09:58:12 -0700

    Complete enumerables exercises

--- ruby_exercises/enumerables/exercises_1/spec/find_spec.rb
+++ ruby_exercises/enumerables/exercises_1/spec/find_spec.rb
@@ -1,67 +1,68 @@
-RSpec.describe 'find test' do
-
-  it 'first seven letter word' do
-    words = ["capricious", "berry", "unicorn", "bag", "apple", "festering", "pretzel", "pencil"]
+RSpec.describe "find test" do # rubocop:disable Metrics/BlockLength
+  it "first seven letter word" do
+    words = %w[capricious berry unicorn bag apple festering
+               pretzel pencil]
     found = words.find do |word|
       word.length == 7
     end
     expect(found).to eq("unicorn")
   end
 
-  xit 'no waldo' do
-    words = ["scarf", "sandcastle", "flag", "pretzel", "crow", "key"]
+  it "no waldo" do
+    words = %w[scarf sandcastle flag pretzel crow key]
     found = words.find do |word|
-      # Your code goes here
+      word == "waldo"
     end
     expect(found).to eq(nil)
   end
 
-  xit 'found waldo' do
-    words = ["noise", "dog", "fair", "house", "waldo", "bucket", "fish"]
-    # Your code goes here
+  it "found waldo" do
+    words = %w[noise dog fair house waldo bucket fish]
+    found = words.find do |word|
+      word == "waldo"
+    end
     expect(found).to eq("waldo")
   end
 
-  xit 'no three letter words' do
-    words = ["piglet", "porridge", "bear", "blueberry"]
+  it "no three letter words" do
+    words = %w[piglet porridge bear blueberry]
     # Your code goes here
     expect(found).to eq(nil)
   end
 
-  xit 'find 13' do
+  it "find 13" do
     numbers = [2, 13, 19, 8, 3, 27]
     # Your code goes here
     expect(found).to eq(13)
   end
 
-  xit 'find first even number' do
+  it "find first even number" do
     numbers = [3, 7, 13, 11, 10, 2, 17]
     # Your code goes here
     expect(found).to eq(10)
   end
 
-  xit 'first multiple of 3' do
+  it "first multiple of 3" do
     numbers = [2, 8, 9, 27, 24, 5]
     # Your code goes here
     expect(found).to eq(9)
   end
 
-  xit 'first word starting with q' do
-    words = ["weirdo", "quill", "fast", "quaint", "quitter", "koala"]
+  it "first word starting with q" do
+    words = %w[weirdo quill fast quaint quitter koala]
     # Your code goes here
     expect(found).to eq("quill")
   end
 
-  xit 'first word ending with er' do
-    words = ["biggest", "pour", "blight", "finger", "pie", "border"]
+  it "first word ending with er" do
+    words = %w[biggest pour blight finger pie border]
     # Your code goes here
     expect(found).to eq("finger")
   end
 
-  xit 'first number greater than 20' do
+  it "first number greater than 20" do
     numbers = [1, 8, 19, 21, 29, 31, 34]
     # Your code goes here
     expect(found).to eq(21)
   end
 end
-
