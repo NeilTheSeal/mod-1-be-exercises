a483e261dc0a17cd4ed9d2cc74aa40e4c5a92e2d
NeilTheSeal <neil.hendren@gmail.com>
2024-02-16 10:58:16 -0700

    Work through cities lived in exercise

--- lessons/nested_iteration/lesson/exercise/cities_lived_in.rb
+++ lessons/nested_iteration/lesson/exercise/cities_lived_in.rb
@@ -1,31 +1,34 @@
-cities_lived_in = {
-    michaela: ["Philadelphia", "Fort Collins", "Seattle"],
-    mike: ["Denver", "Santa Fe", "Philadelphia", "Portland"],
-    pamela: ["Lansing"],
-    alex: ["Seattle", "Columbus", "Philadelphia", "Austin"]
-}
+# cities_lived_in = {
+#   michaela: ["Philadelphia", "Fort Collins", "Seattle"],
+#   mike: ["Denver", "Santa Fe", "Philadelphia", "Portland"],
+#   pamela: ["Lansing"],
+#   alex: %w[Seattle Columbus Philadelphia Austin]
+# }
 
-
-# Problem #1: 
-# Get a unique list of all of the cities that these humans have lived in  
+# Problem #1:
+# Get a unique list of all of the cities that these humans have lived in
 # ["Philadelphia", "Fort Collins", "Seattle", "Denver", "Santa Fe", "Portland", "Lansing", "Columbus", "Austin"]
 
+# cities_list = []
 
+# cities_lived_in.each do |_person, cities|
+#   cities.each do |city|
+#     cities_list << city unless cities_list.include?(city)
+#   end
+# end
 
+# p cities_list
 
-# Problem #2: 
-# Write code that iterates through the `cities_lived_in` hash, and returns a list of  
+# Problem #2:
+# Write code that iterates through the `cities_lived_in` hash, and returns a list of
 # names of the humans who've lived in Philadelphia.
 
 # [:michaela, :mike, :alex]
 # or
 # ["Michaela", "Mike", "Alex"]
 
-
-
-
-# Problem #3: 
-# Create a hash that has the city as a key, and the number of people that live in it as it's value: 
+# Problem #3:
+# Create a hash that has the city as a key, and the number of people that live in it as it's value:
 
 # {
 #     "Philadelphia" => 3,
@@ -38,3 +41,15 @@ cities_lived_in = {
 #     "Columbus => 1,
 #     "Austin" => 1
 # }
+
+cities_lived_in = {
+  michaela: ["Philadelphia", "Fort Collins", "Seattle"],
+  mike: ["Denver", "Santa Fe", "Philadelphia", "Portland"],
+  pamela: ["Lansing"],
+  alex: %w[Seattle Columbus Philadelphia Austin]
+}
+
+cities_list = Hash.new(0)
+cities_lived_in.each_value { |a| a.each { |c| cities_list[c] += 1 } }
+
+p cities_list.sort.to_h
