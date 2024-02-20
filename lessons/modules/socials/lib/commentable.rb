afa36774b97f517f55f8c4c1ad18f4861a4fa9a7
NeilTheSeal <neil.hendren@gmail.com>
2024-02-20 10:49:38 -0700

    Finish modules lesson exercises

--- lessons/modules/socials/lib/commentable.rb
+++ lessons/modules/socials/lib/commentable.rb
@@ -1,4 +1,39 @@
 # ./commentable.rb
 module Commentable
-  # what extracted code goes here?
+  def add_comment(comment)
+    comments[next_comment_id] = comment
+  end
+
+  def remove_comment(id)
+    comments.delete(id)
+  end
+
+  def next_comment_id
+    if total_comments.zero?
+      total_comments + 1
+    else
+      comments.keys.last + 1
+    end
+  end
+
+  def total_comments
+    comments.count
+  end
+
+  def display # rubocop:disable Metrics/MethodLength
+    display_string = "#{'-' * 50}\n"
+    if @body
+      display_string += "STATUS UPDATE\n"
+      display_string += "Body: #{body}\n"
+    else
+      display_string += "PHOTO\n"
+      display_string += "URL: #{image_source}\n"
+      display_string += "Caption: #{caption}\n"
+    end
+    comments.each do |id, text|
+      display_string += "#{id}: #{text}\n"
+    end
+    display_string += "-" * 50
+    display_string
+  end
 end
