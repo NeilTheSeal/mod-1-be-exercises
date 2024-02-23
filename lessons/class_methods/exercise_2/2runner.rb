dd75b786903ac9b8e3b584f887e792c7c75c49d9
NeilTheSeal <neil.hendren@gmail.com>
2024-02-23 09:49:43 -0700

    Play around

--- lessons/class_methods/exercise_2/2runner.rb
+++ lessons/class_methods/exercise_2/2runner.rb
@@ -1,17 +1,16 @@
-require './lib/user'
-require 'pry'
+require "./lib/user"
+require "pry"
 
 ## Create one instance of a User, and say hello to that user.
 user = User.new("Parker")
 puts user.say_hello
 
-
 ## Create many instances of Users, and see what they look like in Pry.
 users = [
-    {name: "Sal"},
-    {name: "Brian"},
-    {name: "Megan"},
-  ]
+  { name: "Sal" },
+  { name: "Brian" },
+  { name: "Megan" }
+]
 
 user_objects = User.create_multiple(users)
-#binding.pry ## What is user_objects?
+# binding.pry ## What is user_objects?
