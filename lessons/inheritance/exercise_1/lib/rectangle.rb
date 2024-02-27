22cd191463b3525b0a0fa188ea3088067316d634
NeilTheSeal <neil.hendren@gmail.com>
2024-02-27 10:09:31 -0700

    Complete shapes inheritance exercise

--- lessons/inheritance/exercise_1/lib/rectangle.rb
+++ lessons/inheritance/exercise_1/lib/rectangle.rb
@@ -1,12 +1,2 @@
-class Rectangle
-
-  # this class should have the following properties:
-  # - color
-  # - length
-  # - width
-
-  #It should have the following methods:
-  # - area (width * length)
-  # - perimeter (2 * (length + width))
-
+class Rectangle < Shape
 end
--- lessons/inheritance/exercise_1/lib/shape.rb
+++ lessons/inheritance/exercise_1/lib/shape.rb
@@ -1,11 +1,17 @@
 class Shape
+  attr_reader :length, :width, :color
 
-  # This class should be a parent that creates the following properties:
-  # - color
-  # - width
-  # - length
-  # It should also have the following behaviors:
-  # - area (width * length)
-  # - perimeter (2 * (length + width))
+  def initialize(color, length, width)
+    @color = color
+    @length = length
+    @width = width
+  end
 
+  def area
+    @length * @width
+  end
+
+  def perimeter
+    2 * @length + 2 * @width
+  end
 end
